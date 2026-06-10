531cd1db81dbd7710adc2f68dd05a1cda854fbf0
Hiba <khiba9543@gmail.com>
2026-06-10 21:36:30 +0530

    Permutation in Strings

--- Week_2/Day_2/Permutation_in_String.java
+++ Week_2/Day_2/Permutation_in_String.java
@@ -1,3 +1,7 @@
+//Problem Link: https://leetcode.com/problems/permutation-in-string/
+//Description: Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise. In other words, return true if one of s1's permutations is the substring of s2.
+//Approach: We can use a sliding window approach to check for permutations of s1 in s2. We maintain two frequency arrays for characters in s1 and the current window in s2. As we slide the window, we update the frequency array for s2 and compare it with the frequency array for s1.
+//Time Complexity: O(n) where n is the length of s2.
 package Week_2.Day_2;
 
 class Solution {
