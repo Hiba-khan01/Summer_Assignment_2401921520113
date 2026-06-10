d01f1cad3861b0d72190f382ae33948507945992
Hiba <khiba9543@gmail.com>
2026-06-10 21:14:57 +0530

    Longest Substring without repeating chars

--- Week_2/Day_2/LongestSubstringWithoutRepeatingChar.java
+++ Week_2/Day_2/LongestSubstringWithoutRepeatingChar.java
@@ -1,7 +1,7 @@
 //Problem: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 //Description: Given a string s, find the length of the longest substring without repeating characters.
 //Approach: We can use a sliding window approach with two pointers (left and right) to keep track of the current substring. We also use a HashSet to store the characters in the current substring. If we encounter a repeating character, we move the left pointer until we remove the duplicate character from the set.
-//Time Complexity: O(n) 
+//Time Complexity: O(n)
 package Week_2.Day_2;
 import java.util.HashSet;
 class Solution {
