c63340f6d16f2dd4b8938519ffb0ebf9771d366b
Hiba <khiba9543@gmail.com>
2026-06-11 23:32:04 +0530

    Decode String

--- Week_2/Day_4/DecodeString.java
+++ Week_2/Day_4/DecodeString.java
@@ -1,3 +1,51 @@
+// Problem: https://leetcode.com/problems/decode-string/
+// Description: Decode the encoded string following the k[encoded_string] pattern.
+// Approach: Use recursion to process nested brackets and repeat substrings.
+// Time Complexity: O(n)
 package Week_2.Day_4;
 
 
+class Solution {
+    int idx = 0;
+
+    public String decodeString(String s) {
+        return solve(s);
+    }
+
+    private String solve(String s) {
+        String ans = "";
+        int num = 0;
+
+        while (idx < s.length()) {
+            char ch = s.charAt(idx);
+
+            if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                idx++;
+
+                String part = solve(s);
+
+                for (int i = 0; i < num; i++) {
+                    ans += part;
+                }
+
+                num = 0;
+            }
+
+            else if (ch == ']') {
+                return ans;
+            }
+
+            else {
+                ans += ch;
+            }
+
+            idx++;
+        }
+
+        return ans;
+    }
+}
