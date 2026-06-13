abc409ae1c17ee53e7973a386eb84f3474ce891c
Hiba <khiba9543@gmail.com>
2026-06-13 20:59:59 +0530

    Group Anagrams

---
+++ Week_2/Day_5/GroupAnagram.java
@@ -0,0 +1,27 @@
+// Problem: Group Anagrams
+// Description: Group strings that are anagrams of each other.
+// Approach: Sort each string and use the sorted string as a HashMap key.
+// Time Complexity: O(n * k log k)
+// Space Complexity: O(n * k)
+package Week_2.Day_5;
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String key = new String(ch);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
