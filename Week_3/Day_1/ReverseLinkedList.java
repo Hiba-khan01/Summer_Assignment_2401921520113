0faf872f0c1d825eb2f717c3168942770f7d60e2
Hiba <khiba9543@gmail.com>
2026-06-15 23:59:43 +0530

    Reverse Linked List added

--- Week_3/Day_1/ReverseLinkedList.java
+++ Week_3/Day_1/ReverseLinkedList.java
@@ -1,5 +1,22 @@
+//Problem: https://leetcode.com/problems/reverse-linked-list/
+// Description: Reverse the linked list and return the new head.
+// Approach: Iteratively , here we reverse links using previous and next pointers.
+// Time Complexity: O(n)
 package Week_3.Day_1;
+import java.util.List;
 
-public class ReverseLinkedList {
-    
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+
+        while (temp != null) {
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        return prev;
+    }
 }
