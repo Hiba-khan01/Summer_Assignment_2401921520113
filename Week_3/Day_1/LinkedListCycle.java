cca0f5d4b46a8bd79f9e4077b0d8ba8058422291
Hiba <khiba9543@gmail.com>
2026-06-15 23:56:51 +0530

    Linked List Cycle added

--- Week_3/Day_1/LinkedListCycle.java
+++ Week_3/Day_1/LinkedListCycle.java
@@ -1,5 +1,24 @@
+// Problem: https://leetcode.com/problems/linked-list-cycle/
+// Description: Detect whether a linked list contains a cycle.
+// Approach: we use slow and fast pointers (Floyd Cycle Detection).
+// Time Complexity: O(n)
 package Week_3.Day_1;
 
-public class LinkedListCycle {
-    
+import java.util.List;
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = head;
+
+        while (temp != null && temp.next != null) {
+            prev = prev.next;
+            temp = temp.next.next;
+
+            if (prev == temp) return true;
+        }
+
+        return false;
+    }
 }
+
