7c1f51941776031e636f44abe0a06017479e7714
Hiba <khiba9543@gmail.com>
2026-06-15 23:34:46 +0530

    Test added

--- Week_2/OOPs/Test.java
+++ Week_2/OOPs/Test.java
@@ -1,5 +1,28 @@
 package Week_2.OOPs;
 
+package live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
 public class Test {
-    
+
+    public static void main(String[] args) {
+
+        // Part A
+        Veena veena = new Veena();
+        veena.play();
+
+        // Part B
+        Saxophone saxophone = new Saxophone();
+        saxophone.play();
+
+        // Part C
+        Playable p1 = new Veena();
+        Playable p2 = new Saxophone();
+
+        p1.play();
+        p2.play();
+    }
 }
