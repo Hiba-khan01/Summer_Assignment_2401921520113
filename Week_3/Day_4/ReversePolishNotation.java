3050c2a9526e04f1fd7cc429fef847237dfe5001
Hiba <khiba9543@gmail.com>
2026-06-19 22:05:36 +0530

    Evaluate Reverse Polish Notation added

--- Week_3/Day_4/ReversePolishNotation.java
+++ Week_3/Day_4/ReversePolishNotation.java
@@ -1,2 +1,45 @@
+//Problem: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+// Description: Evaluate an arithmetic expression using a stack.
+// Approach:first, Push numbers and then apply operators on top elements.
+// Time Complexity: O(n)
 package Week_3.Day_4;
+import java.util.*;
 
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+
+        for(String s : tokens){
+
+            if(s.equals("+")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a + b);
+            }
+
+            else if(s.equals("-")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a - b);
+            }
+
+            else if(s.equals("*")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a * b);
+            }
+
+            else if(s.equals("/")){
+                int b = st.pop();
+                int a = st.pop();
+                st.push(a / b);
+            }
+
+            else{
+                st.push(Integer.parseInt(s));
+            }
+        }
+
+        return st.peek();
+    }
+}
