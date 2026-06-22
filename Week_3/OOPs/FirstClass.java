0ab1d35b9ab602a83472535cbecd76dbb1c42a14
Hiba <khiba9543@gmail.com>
2026-06-22 22:58:23 +0530

    First Class added

--- Week_3/OOPs/FirstClass.java
+++ Week_3/OOPs/FirstClass.java
@@ -1,1 +1,9 @@
+package Week_3.OOPs;
 
+// First Class compartment implementation.
+
+class FirstClass extends Compartment {
+    public String notice() {
+        return "First Class Compartment";
+    }
+}
