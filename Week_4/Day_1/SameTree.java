0bb05e09aa9a5859c174c7fe86101d7100d39598
Hiba <khiba9543@gmail.com>
2026-06-22 23:18:52 +0530

    Same Tree added

--- Week_4/Day_1/SameTree.java
+++ Week_4/Day_1/SameTree.java
@@ -1,2 +1,19 @@
-package Week_4.Day_1;
+//Problem: https://leetcode.com/problems/same-tree/
+// Description: Check if two binary trees are identical.
+// Approach: first we Compare current nodes and recursively check subtrees.
+// Time Complexity: O(n)
 
+package Week_4.Day_1;
+import java.util.*;
+
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+
+        if (p.val != q.val) return false;
+
+        return isSameTree(p.left, q.left) &&
+               isSameTree(p.right, q.right);
+    }
+}
