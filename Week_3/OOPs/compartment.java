c1632a20a2c83d0e6799fcdcb5024f54cc7d547e
Hiba <khiba9543@gmail.com>
2026-06-22 23:05:02 +0530

    Compartment added

--- Week_3/OOPs/compartment.java
+++ Week_3/OOPs/compartment.java
@@ -1,7 +1,7 @@
-package Week_3.OOPs;
-
 // Abstract class representing a railway compartment.
 // Contains abstract notice() method to be overridden by child classes.
+
+package Week_3.OOPs;
 
 abstract class Compartment {
     public abstract String notice();
