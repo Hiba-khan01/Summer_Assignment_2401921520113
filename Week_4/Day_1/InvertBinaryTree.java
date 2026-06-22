f0579e5963de06d688d810d94e972a04db365e3a
Hiba <khiba9543@gmail.com>
2026-06-22 23:13:19 +0530

    Invert Binary Tree added

--- Week_4/Day_1/InvertBinaryTree.java
+++ Week_4/Day_1/InvertBinaryTree.java
@@ -1,5 +1,22 @@
+//Problem: https://leetcode.com/problems/invert-binary-tree/
+// Description: Invert a binary tree.
+// Approach: Swap left and right child recursively.
+// Time Complexity: O(n)
+
 package Week_4.Day_1;
 
-public class InvertBinaryTree {
-    
+import java.util.*;
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return null;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.right);
+        invertTree(root.left);
+
+        return root;
+    }
 }
