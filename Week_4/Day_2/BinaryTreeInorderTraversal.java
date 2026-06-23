4583fae1d93fe59414cf51ca3c1c2f0412b40f6e
Hiba <khiba9543@gmail.com>
2026-06-23 22:30:06 +0530

    Binary Tree Inorder Traversal added

--- Week_4/Day_2/BinaryTreeInorderTraversal.java
+++ Week_4/Day_2/BinaryTreeInorderTraversal.java
@@ -1,5 +1,22 @@
+//Problem: https://leetcode.com/problems/binary-tree-inorder-traversal/
+//Description: Given the root of a binary tree, return the inorder traversal of its nodes' values.
+//Approach: Traverse the left subtree, visit the root, then traverse the right subtree (Left → Root → Right).
+//Time Complexity: O(n)
 package Week_4.Day_2;
+import java.util.*;
 
-public class BinaryTreeInorderTraversal {
-    
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        inorder(root, ans);
+        return ans;
+    }
+
+    private void inorder(TreeNode root, List<Integer> ans) {
+        if (root == null) return;
+
+        inorder(root.left, ans);
+        ans.add(root.val);
+        inorder(root.right, ans);
+    }
 }
