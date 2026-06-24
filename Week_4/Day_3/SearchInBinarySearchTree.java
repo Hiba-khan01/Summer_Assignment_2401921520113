170c54102aac4d6b2b39cb90fe759a2ded6beb5b
Hiba <khiba9543@gmail.com>
2026-06-24 23:03:16 +0530

    Search in Binary Search Tree added

--- Week_4/Day_3/SearchInBinarySearchTree.java
+++ Week_4/Day_3/SearchInBinarySearchTree.java
@@ -1,1 +1,19 @@
+//Problem: https://leetcode.com/problems/search-in-a-binary-search-tree/
+//Description: Given the root of a Binary Search Tree (BST) and a value, return the subtree rooted at the node with that value. If the value does not exist, return null.
+//Approach: Compare the target value with the current node; move left if smaller, right if larger, until the value is found or the tree ends.
+//Time Complexity: O(h)
 package Week_4.Day_3;
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val)
+                return root;
+            else if (val < root.val)
+                root = root.left;
+            else
+                root = root.right;
+        }
+        return null;
+    }
+}
