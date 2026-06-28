f1024b49631cc1e91580dd6e097765334a8f4202
Hiba <khiba9543@gmail.com>
2026-06-28 13:12:27 +0530

    Arithmetic added

--- Week_4/OOPs/Arithmetic.java
+++ Week_4/OOPs/Arithmetic.java
@@ -1,5 +1,12 @@
 package Week_4.OOPs;
 
-public class Arithmetic {
-    
+// Implements Test interface.
+// Returns square of a number.
+
+class Arithmetic implements Test {
+
+    public int square(int n) {
+        return n * n;
+    }
+  
 }
