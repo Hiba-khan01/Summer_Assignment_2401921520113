0a98da04b1668bf0a35d36de600b391e6291b545
Hiba <khiba9543@gmail.com>
2026-06-28 13:18:27 +0530

    Point Demo added

--- Week_4/OOPs/PointDemo.java
+++ Week_4/OOPs/PointDemo.java
@@ -1,5 +1,14 @@
 package Week_4.OOPs;
 
+// Tests Point class methods.
+
 public class PointDemo {
-    
+
+    public static void main(String[] args) {
+        Point p = new Point();
+
+        p.setXY(10, 20);
+        p.display();
+    }
+  
 }
