95a07eda1d6b79a1782a6df3ebed93f0cdada716
Hiba <khiba9543@gmail.com>
2026-06-28 12:37:04 +0530

    Binary Tree Maximum Path Sum added

--- Week_4/Day_4/BinaryTreeMaxPathSum.java
+++ Week_4/Day_4/BinaryTreeMaxPathSum.java
@@ -1,5 +1,26 @@
+//Problem: https://leetcode.com/problems/binary-tree-maximum-path-sum/
+// Description: Find the maximum path sum in a binary tree.
+// Approach: here, recursively compute maximum gain from each node and update answer.
+// Time Complexity: O(n)
+
 package Week_4.Day_4;
 
-public class BinaryTreeMaxPathSum {
-    
+class Solution {
+    int max = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+
+        max = Math.max(max, root.val + left + right);
+
+        return root.val + Math.max(left, right);
+    }
 }
