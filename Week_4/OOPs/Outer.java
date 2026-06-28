90dda54b49c40eb5a319bbd78daf1d43ed3dd3c2
Hiba <khiba9543@gmail.com>
2026-06-28 13:16:41 +0530

    Outer added

--- Week_4/OOPs/Outer.java
+++ Week_4/OOPs/Outer.java
@@ -1,5 +1,18 @@
 package Week_4.OOPs;
 
-public class Outer {
-    
+// Outer class with display().
+// Contains Inner class with its own display().
+
+class Outer {
+
+    void display() {
+        System.out.println("Outer class");
+    }
+
+    class Inner {
+        void display() {
+            System.out.println("Inner class");
+        }
+    }
+  
 }
