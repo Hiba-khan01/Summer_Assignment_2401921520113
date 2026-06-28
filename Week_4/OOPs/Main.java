0912aefc4be4158aefc2b56f4a236c80c617c9c0
Hiba <khiba9543@gmail.com>
2026-06-28 13:15:45 +0530

    Main added

--- Week_4/OOPs/Main.java
+++ Week_4/OOPs/Main.java
@@ -1,5 +1,16 @@
 package Week_4.OOPs;
 
+// Creates objects of Outer and Inner.
+// Calls both display() methods.
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        Outer o = new Outer();
+        o.display();
+
+        Outer.Inner i = o.new Inner();
+        i.display();
+    }
+  
 }
