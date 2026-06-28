186ec7a4d18d827c3d2963c8a65e8cd862e81296
Hiba <khiba9543@gmail.com>
2026-06-28 13:20:37 +0530

    Test Box added

--- Week_4/OOPs/TestBox.java
+++ Week_4/OOPs/TestBox.java
@@ -1,5 +1,14 @@
 package Week_4.OOPs;
 
+// Creates Box3D object.
+// Displays area and volume.
+
 public class TestBox {
-    
+    public static void main(String[] args) {
+
+        Box3D b = new Box3D(5, 4, 3);
+
+        System.out.println("Area = " + b.area());
+        System.out.println("Volume = " + b.volume());
+    }
 }
