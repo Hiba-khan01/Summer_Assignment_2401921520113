7c63b88157efaeeb5ca172db357a0bacab1e2de0
Hiba <khiba9543@gmail.com>
2026-06-28 13:14:21 +0530

    Box3D added

--- Week_4/OOPs/Box3D.java
+++ Week_4/OOPs/Box3D.java
@@ -1,5 +1,18 @@
 package Week_4.OOPs;
 
-public class Box3D {
-    
+// Extends Box.
+// Calculates volume.
+
+class Box3D extends Box {
+    int height;
+
+    Box3D(int l, int b, int h) {
+        super(l, b);
+        height = h;
+    }
+
+    int volume() {
+        return length * breadth * height;
+    }
+  
 }
