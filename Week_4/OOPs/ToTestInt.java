ba14a96bdcac416fe745ab3bc403f4df5d6af744
Hiba <khiba9543@gmail.com>
2026-06-28 13:21:58 +0530

    ToTest Int added

--- Week_4/OOPs/ToTestInt.java
+++ Week_4/OOPs/ToTestInt.java
@@ -1,5 +1,14 @@
 package Week_4.OOPs;
 
+// Creates Arithmetic object.
+// Calls square() method.
+
 public class ToTestInt {
-    
+
+    public static void main(String[] args) {
+        Arithmetic obj = new Arithmetic();
+      
+        System.out.println(obj.square(5));
+    }
+  
 }
