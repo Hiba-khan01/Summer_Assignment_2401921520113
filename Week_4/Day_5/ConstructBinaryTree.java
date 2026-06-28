ee7901cb59d260c6e93975f6c1af160d02b5c54c
Hiba <khiba9543@gmail.com>
2026-06-28 12:58:12 +0530

    Construct Binary Tree added

--- Week_4/Day_5/ConstructBinaryTree.java
+++ Week_4/Day_5/ConstructBinaryTree.java
@@ -1,5 +1,34 @@
-package Week_4.Day_5;
+//Problem: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+// Description: Construct a binary tree from preorder and inorder traversals.
+// Approach: we just use preorder for root and hashmap for inorder positions.
+// Time Complexity: O(n)
 
-public class ConstructBinaryTree {
-    
+package Week_4.Day_5;
+import java.util.*;
+
+class Solution {
+    int p = 0;
+    HashMap<Integer, Integer> mp = new HashMap<>();
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        for (int i = 0; i < inorder.length; i++) {
+            mp.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] pre, int l, int r) {
+        if (l > r) return null;
+
+        int val = pre[p++];
+        TreeNode root = new TreeNode(val);
+
+        int mid = mp.get(val);
+
+        root.left = build(pre, l, mid - 1);
+        root.right = build(pre, mid + 1, r);
+
+        return root;
+    }
 }
